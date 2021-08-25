c0d72c9adb6d61c555959644d8ff56c24e7b1d65
Richard Requelme <richardrequelme3@gmail.com>
2021-08-25 16:08:27 -0500

    first change

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../components/loguito.png'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#040720'}}>
+            <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...'></img></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" >
+                <ul className="navbar-nav ml-auto">
+
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+        
+    )
+}
+
+
+export default Navbar;
