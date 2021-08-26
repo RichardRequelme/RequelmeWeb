e5104dbef17a50c789c7da47ba78ff355ad1cab0
Richard Requelme <richardrequelme3@gmail.com>
2021-08-26 16:47:22 -0500

    Navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import logo from '../components/loguito.png'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: '#040720'}}>
+        <div className="container">
+            
             <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...'></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <FontAwesomeIcon icon={faBars} style={{color:'white'}}/>
             </button>
 
             <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" >
                 <ul className="navbar-nav ml-auto">
 
                     <li className="nav-item active">
-                        <a className="nav-link" href="#" id="navegador">Home</a>
+                        <a className="nav-link" href="#" id="navegador">articles()</a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#" id="navegador">Link</a>
+                        <a className="nav-link" href="#" id="navegador">projects()</a>
                     </li>
-
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#" id="navegador">aboutMe()</a>
+                    </li>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#" id="navegador">contact()</a>
+                    </li>
+                    
                 </ul>
             </div>
+        </div>
         </nav>
         
     )
